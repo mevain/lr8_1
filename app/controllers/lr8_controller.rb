1c4f8c4ee07bd7d130de7570d81c7a91d18f0926
Ksenia <xen.bardikina@yandex.ru>
2022-11-21 01:59:49 +0300

    new method

--- app/controllers/lr8_controller.rb
+++ app/controllers/lr8_controller.rb
@@ -3,5 +3,14 @@ class Lr8Controller < ApplicationController
   end
 
   def output
+    @amount = params[:amount]
+    @array = params[:array]
+    @result = @array.split(" ").select{|i| perfect(i.to_i)}
+    #@result = (1..(6 / 2)).to_a
+    perfect(6)
+  end
+
+  def perfect(a)
+    return (1..(a / 2)).to_a.select{|i| a % i == 0}.sum.eql? a
   end
 end
