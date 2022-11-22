ba743e8febb0e4d6cb14372c7c4ba7bac41466d1
Ksenia <xen.bardikina@yandex.ru>
2022-11-22 08:50:16 +0300

    function

--- app/controllers/lr8_controller.rb
+++ app/controllers/lr8_controller.rb
@@ -5,12 +5,21 @@ class Lr8Controller < ApplicationController
   def output
     @amount = params[:amount]
     @array = params[:array]
-    @result = @array.split(" ").select{|i| perfect(i.to_i)}
-    #@result = (1..(6 / 2)).to_a
-    perfect(6)
+    @result = @array.split(" ").select{|i| (s = (1..(i.to_i / 2)).to_a.select{|j| i.to_i % j == 0}.sum).eql? i.to_i}
+    @indexs = @result.map{|i| @array.split(" ").index(i)}
+    @cuts = Array.new
+    @cuts[0] = Array.new
+    @cuts[0][0] = @result[0]
+    ind = 0 
+    1.upto(@result.size - 1) do |i|
+      if @indexs[i] != @indexs[i - 1] + 1 then
+        ind += 1 
+        @cuts[ind] = Array.new
+      end
+      @cuts[ind].append(@result[i])
+    end
   end
 
   def perfect(a)
-    return (1..(a / 2)).to_a.select{|i| a % i == 0}.sum.eql? a
   end
 end
