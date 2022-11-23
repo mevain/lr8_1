489067ba5ea599c8be9b3e78503a6cb50b6119bc
Ksenia <xen.bardikina@yandex.ru>
2022-11-23 22:07:29 +0300

    for git hub

--- spec/requests/lr8_spec.rb
+++ spec/requests/lr8_spec.rb
@@ -10,9 +10,13 @@ RSpec.describe "Lr8s", type: :request do
 
   describe "GET /output" do
     it "returns http success" do
-      get "/lr8/output"
+      get "/lr8/output", params: {amount: 6, array: "1 6 28 5 496 13"}
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it 'works correctly' do
+      get "/lr8/output", params: {amount: 6, array: "1 6 28 5 496 13"}
+      expect(assigns(:cuts)).to eq([["6", "28"], ["496"]])
+    end
+  end
 end
